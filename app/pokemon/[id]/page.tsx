28093a7bf62c186f7de8727331d6f16c6e67ae5d
정원준 <mullung.study@gmail.com>
2025-12-12 10:49:05 +0900

    20251212 end

--- app/pokemon/[id]/page.tsx
+++ app/pokemon/[id]/page.tsx
@@ -1,7 +1,21 @@
-import { getPokemon } from "@/lib/pokeAPI"
 import PokemonCard from "@/components/PokemonCard"
 import { notFound } from "next/navigation";
 import { getPokemonById } from "@/lib/actions/pokemonData";
+import { Metadata } from "next";
+
+export async function generateMetadata({params}:{params:Promise<{id:string}>}):Promise<Metadata> {
+  const {id} = await params;
+  const pokemon = await getPokemonById(Number(id));
+
+  if (!pokemon) {
+    return {title: "포켓몬을 찾을 수 없습니다"};
+  }
+
+  return {
+    title: pokemon.name_ko,
+    description: `${pokemon.name_ko}의 상세정보`
+  }
+}
 
 export default async function PokemonDetails({params}:{params:Promise<{id:string}>}) {
 
